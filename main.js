adfcaed310108e60dab5159d55550ea7f26d5e82
ATrader133 <info.abrartraders@gmail.com>
2025-12-16 20:32:23 +0530

    Update main.js

--- main.js
+++ main.js
@@ -362,3 +362,4 @@ document.addEventListener('DOMContentLoaded', function() {
     const yearEl = document.getElementById('currentYear');
     if(yearEl) yearEl.textContent = new Date().getFullYear();
 });
+
